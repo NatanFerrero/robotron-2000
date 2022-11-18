53515509326ebf1bf9dce9a8305fe708967aae81
Nata Correia Santos <nata.correia@totvs.com.br>
2022-11-18 20:18:31 -0300

    const controle. Somar and Subtrair

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-const robotron = document.querySelector("#robotron")
-robotron.addEventListener("click", dizOi)
+const subrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-function dizOi(){
-    alert("oi")
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
+
+function manipulaDados(operacao) {
+    if(operacao === "-"){
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
 }
